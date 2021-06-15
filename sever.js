736d65b75730d7480d71a6b2c6e94c579c8fddbf
bbyoda <h2trungvuong@gmail.com>
2021-06-16 00:40:26 +0200

    prod mode

---
+++ sever.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("./dist/coolschrank-v2"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/coolschrank-v2" });
+});
+
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`);
